8fd9cb1099facd923b076d05aaa6f388a51c7e4e
Jeff Lynch <jlynch3@uw.edu>
2021-09-14 18:44:54 -0700

    first commit

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").router();
 const Workout = require("../models/workout");
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -11,7 +11,7 @@ router.post("/api/workout", ({ body }, res) => {
     });
 });
 
-router.post("/api/workout/bulk", ({ body }, res) => {
+router.post("/api/workouts/bulk", ({ body }, res) => {
   Workout.insertMany(body)
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -21,7 +21,7 @@ router.post("/api/workout/bulk", ({ body }, res) => {
     });
 });
 
-router.get("/api/workout", (req, res) => {
+router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .sort({ date: -1 })
     .then(dbWorkout => {
